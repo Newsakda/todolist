c667bbc8d2c63cf5990d55c505436a9c409a581e
Sakda Butsuwan <sakdabutsuwan@gmail.com>
2024-11-20 21:20:16 +0700

    hookandauth1

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,12 +2,20 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/api(.*)"],
   // Routes that can always be accessed, and have
   // no authentication information
-  ignoredRoutes: ["/api/webhook"]
+  ignoredRoutes: ["/api/webhook", "/_next/static/(.*)"]
 });
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    // Match all request paths except for the ones starting with:
+    // - _next/static (static files)
+    // - _next/image (image optimization files)
+    // - favicon.ico (favicon file)
+    // - public folder
+    "/((?!.+\\.[\\w]+$|_next/static|_next/image|favicon\\.ico).*)",
+    "/"
+  ],
 }; 
