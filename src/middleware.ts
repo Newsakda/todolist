4681c6d7815626d165577d42027fc139f34f43c8
Sakda Butsuwan <sakdabutsuwan@gmail.com>
2024-11-20 21:29:13 +0700

    hookandauth2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,12 @@
 import { authMiddleware } from "@clerk/nextjs";
 
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
 export default authMiddleware({
-  // Routes that can be accessed while signed out
-  publicRoutes: ["/", "/api(.*)"],
-  // Routes that can always be accessed, and have
-  // no authentication information
-  ignoredRoutes: ["/api/webhook", "/_next/static/(.*)"]
+  publicRoutes: ["/"],
+  ignoredRoutes: ["/((?!api|trpc))(_next|.+\\..+)(.*)"],
 });
 
 export const config = {
-  matcher: [
-    // Match all request paths except for the ones starting with:
-    // - _next/static (static files)
-    // - _next/image (image optimization files)
-    // - favicon.ico (favicon file)
-    // - public folder
-    "/((?!.+\\.[\\w]+$|_next/static|_next/image|favicon\\.ico).*)",
-    "/"
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 }; 
